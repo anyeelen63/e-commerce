457e9567f457c867458208d19057905570cd6d2e
anyeelen63 <anyeelen63@gmail.com>
2021-08-22 16:26:19 -0300

    login funcionando falta estilo

--- js/login.js
+++ js/login.js
@@ -2,25 +2,28 @@ document.addEventListener("DOMContentLoaded", function(e){
 });
 
     function validar(){ 
-        let nombre = document.getElementById("nombre").value;
-         let pass = document.getElementById("pass").value;
+        let Email = document.getElementById("Email").value;
+        let Password = document.getElementById("Password").value;
          
-         if (nombre.length < 8 || nombre.length >20 ) {
+         if (Email.length < 8 || Email.length >20 ) {
         alert('El usuario debe estar entre 8 y 20 caracteres');
         return;
     }
-        
-        if(pass.length < 8 || pass.length >20 ) {
+        //validacion del Email segun el largo
+        if(Password.length < 8 || Password.length >20 ) {
         alert('La clave debe estar entre 8 y 20 caracteres');
         return;
+        //validacion de la contraseña segun el largo
     }
       //valida el ingreso de caracteres, de 8 a 20 por defecto, 
       //si no se cumple salta un alert y vuelve a empezar
-    if((nombre !=="")&& (pass!=="")){
+    if((Email !=="")&& (Password!=="")){
         window.location.href="./products.html";
-        //redirige al html de product con ./ porque esta en otra carpeta 
+        //redirige al html de product con ./ porque esta en otra carpeta
+        //se ejecuta con el onclick
     }
     else{
         alert("debe completar los campos");
     }    
+    //alerta para el ingreso de campos vacios
 }
