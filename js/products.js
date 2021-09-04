bf1b2f6aac3a9cff9c7b188672f040ed66983659
anyeelen63 <anyeelen63@gmail.com>
2021-09-04 20:12:08 -0300

    orden de productos definido

--- js/products.js
+++ js/products.js
@@ -1,10 +1,44 @@
+const ORDER_ASC_BY_PROD_COST = "Cost↑";
+const ORDER_DESC_BY_PROD_COST = "Cost↓";
+const ORDER_DESC_BY_PROD_SOLD = "SoldC";
+var currentProductsArray = [];
+var currentSortCriteria = undefined;
+var minCount = undefined;
+var maxCount = undefined;
 var productsArray = [];
-// se crea la variable de products array para poder utilizarla localmente
-function showProductsList(array){
+
+function sortProducts(criteria, array){
+    let result = [];
+    if (criteria === ORDER_DESC_BY_PROD_SOLD){
+        result = array.sort(function(a, b) {
+            if ( a.soldCount > b.soldCount ){ return -1; }
+            if ( a.soldCount < b.soldCount ){ return 1; }
+            return 0;
+        });
+    }else if (criteria === ORDER_ASC_BY_PROD_COST){
+        result = array.sort(function(a, b) {
+            if ( a.cost < b.cost ){ return -1; }
+            if ( a.cost > b.cost ){ return 1; }
+            return 0;
+        });
+    }else if (criteria === ORDER_DESC_BY_PROD_COST){
+        result = array.sort(function(a, b) {
+            if ( a.cost > b.cost ){ return -1; }
+            if ( a.cost < b.cost ){ return 1; }
+            return 0;
+        });
+    }
+    return result;
+}
+
+
+
+function showProductsList(){
     
     let htmlContentToAppend = "";
-    for(let i = 0; i < array.length; i++){
-        let product = array[i];
+    for(let i = 0; i < currentProductsArray.length; i++){
+        let product = currentProductsArray[i];
+
 
             htmlContentToAppend += `
             <a href="product-info.html" class="list-group-item list-group-item-action">
@@ -15,30 +49,45 @@ function showProductsList(array){
                     <div class="col">
                         <div class="d-flex w-100 justify-content-between">
                             <h4 class="mb-1">`+ product.name +`</h4>
-                            <small class="text-muted">` + product.soldCount + ` artículos </small>
-                            <small class="text-muted">` + product.cost + product.currency + ` </small>
+                            <small class="text-muted">` + product.soldCount + ` artículos ` + product.cost + product.currency  + `  </small>
                         </div>
                         <p class="mb-1">` + product.description + `</p>
                     </div>
                 </div>
             </a>
-            `
-        
+                    }`
+                
         document.getElementById("prod-list-container").innerHTML = htmlContentToAppend;
      }
-    //  se obtienen los elementos del listado de productos
 }
-// se utilizo el codigo de categories, 
-// se cambiaron los atributos de entrada segun lo contenido en en json
-// se cambio lo relacionado a cat por prod
+
+function sortAndShowProducts(sortCriteria, productsArray){
+    currentSortCriteria = sortCriteria;
+
+    if(productsArray != undefined){
+        currentProductsArray = productsArray;
+    }
+    
+    currentProductsArray = sortProducts(currentSortCriteria, currentProductsArray);
+
+    showProductsList();
+}
 
 document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(PRODUCTS_URL).then(function(resultObj){
-        if (resultObj.status === "ok")
-            {
-                productsArray = resultObj.data;
-                showProductsList(productsArray);
+        if (resultObj.status === "ok"){
+            sortAndShowProducts(ORDER_ASC_BY_PROD_COST, resultObj.data);
         }
     });
+
+    document.getElementById("sortAsc").addEventListener("click", function(){
+        sortAndShowProducts(ORDER_ASC_BY_PROD_COST);
+    });
+    document.getElementById("sortDesc").addEventListener("click", function(){
+        sortAndShowProducts(ORDER_DESC_BY_PROD_COST);
+    });
+    document.getElementById("sortByCount").addEventListener("click", function(){
+        sortAndShowProducts(ORDER_DESC_BY_PROD_SOLD);
+    });
+    
 });
-// se llama al json de products para mostrar el listado de productos
