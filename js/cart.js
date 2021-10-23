deecf7eb6dd764355fa2ad125853f49685c529fd
anyeelen63 <anyeelen63@gmail.com>
2021-10-23 17:17:06 -0300

    subtotal actualizado

--- js/cart.js
+++ js/cart.js
@@ -1,59 +1,66 @@
-let productosCarrito = []
+let Carrito = [];
 
+//funcion para actualizar el subtotal. se pasa como parametro cantidad,costo, id donde se 
+//quiere insertar el subtotal. con getElement ponemos donde queremos insertar el subtotal
+//actualizado
+function updateSubtotal(cantidad, costo, id){
+    document.getElementById(id).innerHTML = cantidad*costo;
+    updateTotal();
+}
+
+//declarar variable index inicializada en 0
+//se le va sumando 1 a index en cada iteración, se guarda el id para llamarlo luego
+//en el onchange se llama a la funcion de updateSubtotal() con sus parametros
+//se usa this.value para darle el valor del elemento actual
+//se retoma el id para actualizar el valor del subtotal
 function showCarrito(){
+    let index =0;
     let htmlContentToAppend = "";
 
-    //recorro el arreglo de productos extraigo nombre, moneda, costo por unidad, 
-    //el input de la cantidad, agregando el index al id de json 
-    //al valor se le agrega el count, con cuantos productos se arranca
-    //con onchange se llama a la funcion update 
-    //finalmente se hace el costo por la cantidad que viene por defecto 
-    for(let i = 0; i < article.length; i++){
+    for(let article of Carrito){
+        index++;
+        id=index;
+        let subtotal = parseInt(article.count) * parseInt(article.unitCost);
         htmlContentToAppend += `
         
         <tr>
-        <td> <img class="img-fluid" style="max-width:50px" src="${article[i].src}"></td>
-        <td>${article[i].name}</td>
-        <td>${article[i].currency}</td>
-        <td>${article[i].unitCost}</td>
-        <td><input id="cantidad${i}" type="number" 
-        value="${article[i].count}" onchange="updateSubtotal()"></td>
-        <td id="subtotal${i}">${article[i].unitCost * article[i].count}</td>
+        <td> <img class="img-fluid" style="max-width:60px" src="${article.src}"></td>
+        <td>${article.name}</td>
+        <td>${article.currency} </td>
+        <td>${article.unitCost}</td>
+        <td><input type="number" min="1" value=${article.count} 
+        onchange="updateSubtotal(this.value, ${article.unitCost}, ${id})"></td>
+        <td id="${id}" class="subtotal">${subtotal}</td>
         </tr>
-        `;
+        `
     }
-        //para mostrar los productos insercion por innerhtml
-    document.getElementById("insertCarrito").innerHTML += htmlContentToAppend;
-}
-//variables de costo y cantidad para cada producto
-//toma el costo como el costo unitario que viene por json
-//la cantidad la toma del input
-//if para que la cantidad menor a 0 la reasigne a 1
-//llama al subtotal y le cambia el valor por la multiplicacion de costo*cantidad
-//el id subtotal puntualizado con el i para que cada id sea unico, a medida que el id avanza
 
-function updateSubtotal(){
-    let costo = 0;
-    let cantidad = 0;
-    for(let i = 0; i < article.length; i++){
-        costo=article[i].unitCost;
-        cantidad= document.getElementById("cantidad"+i+"").value;
-        if (cantidad<=0){
-            document.getElementById("cantidad"+i+"").value = 1;
-        }
-        else{
-            document.getElementById("subtotal"+i+"").innerText = costo*cantidad;
-        }
-    }
+        document.getElementById("carrito").innerHTML = htmlContentToAppend;
+
 }
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
+
+
 document.addEventListener("DOMContentLoaded", function(){
     getJSONData(CART_INFO_URL).then(function(resultObj){
         if (resultObj.status === "ok")
-        article = resultObj.data.articles;
+        Carrito = resultObj.data.articles;
         showCarrito();
-        updateSubtotal();
+        updateTotal()
     })
 })
+
+
+//se setea el total en 0 para que se reinicie el valor
+//iterar en la coleccion de elementos que usen la clase subtotal
+//se van sumando los subtotales
+//se insertan en html
+function updateTotal(){
+    let total = 0;
+    let subtotales = document.getElementsByClassName("subtotal");
+    for (let iterator of subtotales){
+        total += parseInt(iterator.innerHTML);
+    }
+    document.getElementById("total").innerHTML= "$" + total;
+    document.getElementById("subtotal").innerHTML = "$" + total;
+}
+
